e6325c4033d17d68debd158139d8d66e17ed1a79
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-06-15 09:08:29 +0300

    users - new user

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,30 @@
+import { GridColDef } from "@mui/x-data-grid"
+import "./add.scss"
+
+type Props = {
+    slug:string,
+    columns:GridColDef[];
+    setOpen:React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Add = (props:Props) => {
+  return (
+    <div className="add">
+        <div className="model">
+            <span className="close" onClick={()=>props.setOpen(false)}>X</span>
+            <h1>Add new {props.slug}</h1>
+            <form>
+                {props.columns.filter(item => item.field !== "id" && item.field != "img")
+                    .map( column => (
+                        <div className="item">
+                            <label>{column.headerName}</label>
+                            <input type={column.type} placeholder={column.field}/>
+                        </div>
+                ))}
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Add
